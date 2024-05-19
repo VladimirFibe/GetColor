f79be376af715d4e655287f0910aa8b5ecaa36fc
Vladimir Fibe <motiw@icloud.com>
2024-05-19 21:13:42 +0500

    Initial Commit

---
+++ GetColor/GetColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetColorApp.swift
+//  GetColor
+//
+//  Created by MacService on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GetColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
